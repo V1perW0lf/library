61cd6b2a1cd386521ca8da70c6aff6376c35a344
CJ <11570429+V1perW0lf@users.noreply.github.com>
2023-10-30 16:53:20 -0500

    remove unnecessary getters and setters

--- logic.js
+++ logic.js
@@ -30,34 +30,6 @@ class Book {
         this.read = read;
     }
 
-    get title() {
-        return this._title;
-    }
-    set title(value) {
-        this._title = value;
-    }
-
-    get author() {
-        return this._author;
-    }
-    set author(value) {
-        this._author = value;
-    }
-
-    get pages() {
-        return this._pages;
-    }
-    set pages(value) {
-        this._pages = value;
-    }
-
-    get read() {
-        return this._read;
-    }
-    set read(value) {
-        this._read = value;
-    }
-
     getInfo() {
         return `${title} by ${author}, ${pages} pages, ${read}`;
     }
